7b63543b38aa4ba4d30676791906c6f1aae577a8
riya <riya.ueng@gmail.com>
2025-07-22 15:39:31 +0200

    bis_Aufgabe_02_done

---
+++ woche_04/tag13_conditionals/aufgaben/aufgabe_02/functions_level_1_5/src/main.js
@@ -0,0 +1,12 @@
+function math(a, b) {
+  console.log(`Multiplikation ${a} und ${b}: ${a * b}`)
+  console.log(`Divison ${a} und ${b}: ${a / b}`)
+}
+
+math(10, 2)
+math(30, 20)
+math(100, 100)
+math(5, 0)
+math(5, 0)
+math(45, 173)
+math(1, 1000)
