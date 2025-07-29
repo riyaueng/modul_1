7cd7a6d25aa668f22b67ff57101f3d606a898bcb
riya <riya.ueng@gmail.com>
2025-07-29 17:18:54 +0200

    bonusaufgabe_functions_fertig

--- woche_04/tag13_conditionals/aufgaben/aufgabe_04/functions_3_1/src/main.js
+++ woche_04/tag13_conditionals/aufgaben/aufgabe_04/functions_3_1/src/main.js
@@ -1,12 +1,68 @@
 import "./style.css"
 
-const headlineColor = document.querySelector(".headline1")
+// * Lösungsweg mit onclick nicht möglich ohne Global Scope!
+// * type="module" verhindert den Zugriff
 
-function colorChange1() {
-  headlineColor.style.color = "#6C046C"
-  document.body.style.backgroundColor = "#228B22"
+// const green = "#228B22"
+// const purple = "#6C046C"
+
+// const lightBlue = "#57599e"
+// const yellow = "#DAC24B"
+
+// const darkBlue = "#050730"
+// const orange = "#EB8A36"
+
+// const blue = " #210cAA"
+// const cyan = "#36e2eb"
+
+// function changeColor(headlineColor, bgColor) {
+//   document.querySelector("h1").style.color = headlineColor
+//   document.body.style.backgroundColor = bgColor
+//   return
+// }
+
+// window.changeColor = changeColor
+
+// console.log(changeColor)
+
+// * Lösungweg mit .addEventListener
+
+const green = "#228B22"
+const purple = "#6C046C"
+
+const lightBlue = "#57599e"
+const yellow = "#DAC24B"
+
+const darkBlue = "#050730"
+const orange = "#EB8A36"
+
+const blue = " #210cAA"
+const cyan = "#36e2eb"
+
+const background = document.querySelector("body")
+const headline = document.querySelector("h1")
+const superBall1 = document.querySelector(".circle1")
+const superBall2 = document.querySelector(".circle2")
+const superBall3 = document.querySelector(".circle3")
+const superBall4 = document.querySelector(".circle4")
+
+superBall1.addEventListener("click", function () {
+  setColors(purple, green)
+})
+
+superBall2.addEventListener("click", function () {
+  setColors(yellow, lightBlue)
+})
+
+superBall3.addEventListener("click", function () {
+  setColors(orange, darkBlue)
+})
+
+superBall4.addEventListener("click", function () {
+  setColors(cyan, blue)
+})
+
+function setColors(headlineColor, bgColor) {
+  background.style.backgroundColor = bgColor
+  headline.style.color = headlineColor
 }
-
-// const changer = document.querySelector(".changeClick")
-
-// changer.addEventListener("click", colorChange1)
